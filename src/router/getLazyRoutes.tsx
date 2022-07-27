4b9779af44f1ed2fd2d3473f5a40e5d59419790c
Filip Skoczeń <philipskoczen@gmail.com>
2022-07-27 09:17:30 +0200

    Fix path

--- src/router/getLazyRoutes.tsx
+++ src/router/getLazyRoutes.tsx
@@ -3,7 +3,7 @@ import routerBase from '~/router/routerBase';
 import type { Route } from '~/router/types';
 
 export default function getLazyRoutes(Loading: () => any): Route[] {
-  const pages = import.meta.glob('./pages/**') as Record<string, () => Promise<{ default: any }>>;
+  const pages = import.meta.glob('../pages/**') as Record<string, () => Promise<{ default: any }>>;
 
   return Object.keys(pages).map((path) => {
     const Component = lazy(pages[path]);
--- src/router/getRoutes.tsx
+++ src/router/getRoutes.tsx
@@ -2,7 +2,7 @@ import type { Route } from '~/router/types.d';
 import routerBase from '~/router/routerBase';
 
 export default function getRoutes(Loading: () => any) {
-  const pages = import.meta.glob('./pages/**', { eager: true }) as Record<any, { default: any }>;
+  const pages = import.meta.glob('../pages/**', { eager: true }) as Record<any, { default: any }>;
   const names = Object.keys(pages);
 
   return {
